5c5a622a1752b1ce7b1490b963c25009be1b9a32
shumoku88-bit <shumoku88@protonmail.com>
2026-09-23 12:19:39 +0900

    test(scheduled): register canonical completion publisher

--- tests/test_runner.adb
+++ tests/test_runner.adb
@@ -33,6 +33,7 @@ with Test_Loam_Scheduled_Creation_Writer;
 with Test_Loam_Scheduled_Creation_Refinement;
 with Test_Loam_Scheduled_Completion_Writer;
 with Test_Loam_Scheduled_Completion_Refinement;
+with Test_Loam_Scheduled_Completion_Publisher;
 with Test_Validity;
 with Test_Review;
 with Test_Initializer;
@@ -223,6 +224,11 @@ begin
    if Selected ("Test_Loam_Scheduled_Completion_Refinement") then
       Announce ("Test_Loam_Scheduled_Completion_Refinement");
       Test_Loam_Scheduled_Completion_Refinement.Run;
+   end if;
+
+   if Selected ("Test_Loam_Scheduled_Completion_Publisher") then
+      Announce ("Test_Loam_Scheduled_Completion_Publisher");
+      Test_Loam_Scheduled_Completion_Publisher.Run;
    end if;
 
    if Selected ("Test_Validity") then
